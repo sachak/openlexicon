3cd1f132480cdb8867345dfdf3bb8ecab69b18e4
Jessica Bourgin <jessica.bourgin@gmail.com>
2025-03-10 17:33:47 +0100

    add tag and option to force delete database in admin section

--- openlexicon.org/openlexiconApp/admin.py
+++ openlexicon.org/openlexiconApp/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
-from .models import Database, DatabaseColumn, DatabaseObject
+from .models import Database, DatabaseColumn, DatabaseObject, Tag
 
 class DatabaseAdmin(admin.ModelAdmin):
     list_display = ('name', 'language',)
     ordering = ["name"]
+    actions=['force_delete_selected']
+
+    def get_actions(self, request):
+        actions = super(DatabaseAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def force_delete_selected(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
 
 class DatabaseColAdmin(admin.ModelAdmin):
-    list_display = ('database', 'name', 'code',)
+    list_display = ('database', 'name', 'type',)
     ordering = ["database", "name"]
 
 admin.site.register(Database, DatabaseAdmin)
 admin.site.register(DatabaseColumn, DatabaseColAdmin)
 admin.site.register(DatabaseObject)
+admin.site.register(Tag)
--- openlexicon.org/openlexiconApp/models.py
+++ openlexicon.org/openlexiconApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 from django_ckeditor_5.fields import CKEditor5Field
 
 class ColSize(models.TextChoices):
@@ -30,12 +32,20 @@ class Lang(models.TextChoices):
     DE = "Dutch"
     MULT = "Multiple languages"
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
 class Database(models.Model):
     id = models.AutoField(primary_key=True, db_index=True)
     name = models.CharField(max_length=50, unique=True)
     info = CKEditor5Field(blank=True, null=True) # for tooltip
+    website = models.URLField(blank=True, null=True)
     language = models.CharField(max_length=20, choices=Lang.choices, default=Lang.FR)
     nbRows = models.IntegerField(null=True)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="databases"
+    )
 
     def __str__(self):
         return self.name
@@ -44,20 +54,20 @@ class DatabaseObject(models.Model):
     id = models.AutoField(primary_key=True, db_index=True)
     database = models.ForeignKey(Database, on_delete=models.CASCADE)
     ortho = models.CharField(max_length=50, verbose_name="Word", db_index=True) # db_index speeds up filter and ordering a lot !
-    lemme = models.CharField(max_length=50, verbose_name="Lemme", null=True)
-    cgram = models.CharField(max_length=20, verbose_name="Grammatical category", null=True)
     jsonData = models.JSONField(null=True, db_index=True) # not sure db_index works on jsonField. TODO : retest with standard columns against jsonData. It should be faster with standard columns with db_index, but to which point ?
 
     class Meta:
-        unique_together = ('database', 'ortho', 'cgram', 'lemme',)
+        unique_together = ('database', 'ortho', 'jsonData',)
 
 class DatabaseColumn(models.Model):
     id = models.AutoField(primary_key=True, db_index=True)
     database = models.ForeignKey(Database, on_delete=models.CASCADE)
     code = models.CharField(max_length=50, verbose_name="Code")
     name = models.CharField(max_length=50, verbose_name="Nom")
+    description = models.CharField(max_length=300, null=True, blank=True)
     size = models.CharField(max_length=10, verbose_name="Taille", default=ColSize.MEDIUM)
     type = models.CharField(max_length=10, verbose_name="Type de données", default=ColType.TEXT) # try to assume type if not given ?
+    mandatory = models.BooleanField(default=False)
     min = models.FloatField(null=True)
     max = models.FloatField(null=True)
 
@@ -66,3 +76,10 @@ class DatabaseColumn(models.Model):
 
     class Meta:
         unique_together = ('database', 'code',)
+
+@receiver(pre_delete, sender=Database)
+def handle_tag_delete(sender, instance, *args, **kwargs):
+    tags = instance.tags.all()
+    for tag in tags:
+        if len(tag.databases.all()) <= 1:
+            tag.delete()
